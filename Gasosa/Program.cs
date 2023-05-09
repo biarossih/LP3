d640e20f01fbb0ffe83dd1151b9c33686c2b6f0b
biarossih <101606116+biarossih@users.noreply.github.com>
2023-05-09 17:19:26 -0300

    Add files via upload

---
+++ Gasosa/Program.cs
@@ -0,0 +1,14 @@
+﻿using CtlAbastec;
+
+var abastecimentos = new Abastecimentos ("Renato Fernandez", 0m);
+var gasolina = new Gasolina("Gasolina", 0);
+
+gasolina.AtualizarValor(20, DateTime.Now, "Gasolina Comum");
+gasolina.AtualizarValor(50, DateTime.Now, "Gasolina Aditivada");
+gasolina.ExecutarAjustes();
+Console.WriteLine(gasolina.EmitirRelatorio());
+
+var diesel = new Diesel("Diesel", 0, -90);
+diesel.AtualizarValor(500, DateTime.Now, "Diesel");
+diesel.ExecutarAjustes();
+Console.WriteLine(diesel.EmitirRelatorio());

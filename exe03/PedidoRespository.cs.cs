588df4386ff0df9aedfd3b72f2a39857cdc19eb7
biarossih <101606116+biarossih@users.noreply.github.com>
2023-06-02 03:10:39 -0300

    PedidoRespository.cs

--- exe03/PedidoRespository.cs.cs
+++ exe03/PedidoRespository.cs.cs
@@ -2,84 +2,83 @@ using DBlesson.Database;
 using DBlesson.Models;
 using Microsoft.Data.Sqlite;
 
-namespace DBlesson.Repositories;
+namespace exe03.Models;
 
-class OrderRepository {
+class PedidoRepository {
     private readonly DatabaseConfig _databaseConfig;
 
-    public OrderRepository(DatabaseConfig databaseConfig) {
+    public PedidoRepository(DatabaseConfig databaseConfig) {
         _databaseConfig = databaseConfig;
     }
 
-    public List<Order> GetAll() {
-        var orders = new List<Order>();
+    public List<Pedido> GetAll() {
+        var pedidos = new List<Pedido>();
 
         var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Order_tb";
+        command.CommandText = "SELECT * FROM Pedidos";
 
         var reader = command.ExecuteReader();
 
         while(reader.Read()) {
-            var id = reader.GetInt32(0);
-            var address = reader.GetString(1);
-            var city =  reader.GetString(2);
-            var region = reader.GetString(3);
-            var zipcode = reader.GetString(4);
-            var country = reader.GetString(5);
-            var phone = reader.GetString(6);
-            var Order = ReaderToOrder(reader);
-            orders.Add(Order);
+            var pedId = reader.GetInt32(0);
+            var enderecoId = reader.GetString(1);
+            var dataPedido =  reader.GetString(2);
+            var peso = reader.GetString(3);
+            var codigoTransportadora = reader.GetString(4);
+            var pedidoClienteId = reader.GetString(5);
+            var Pedido = ReaderToPedido(reader);
+            pedidos.Add(pedido);
         }
 
         connection.Close();
-        return orders;
+        return pedidos;
     }
 
-    public Order Save(Order order) {
+    public Pedido Save(Pedido pedido) {
         var connection =  new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Order_tb VALUES($order_id, $employee_id, $order_date, $order_weigth, $carrier_code, $customer_id)";
-        command.Parameters.AddWithValue("$order_id", order.OrderId);
-        command.Parameters.AddWithValue("$employee_id", order.EmployeeId);
-        command.Parameters.AddWithValue("$order_date", order.OrderDate);
-        command.Parameters.AddWithValue("$order_weigth", order.Weigth);
-        command.Parameters.AddWithValue("$carrier_code", order.CarrierCode);
-        command.Parameters.AddWithValue("$customer_id", order.CustomerId);
+        command.CommandText = "INSERT INTO Pedidos VALUES($pedId, $enderecoId, $dataPedido, $peso, $codigoTransportadora)";
+        command.Parameters.AddWithValue("$pedId", pedido.PedidoId);
+        command.Parameters.AddWithValue("$enderecoId", pedido.EnderecoId);
+        command.Parameters.AddWithValue("$dataPedido", pedido.DataPedido);
+        command.Parameters.AddWithValue("$peso", pedido.Peso);
+        command.Parameters.AddWithValue("$codigoTransportadora", pedido.CodTransportadora);
+        command.Parameters.AddWithValue("$customer_id", pedidoClienteId.PedidoClienteId);
 
         command.ExecuteNonQuery();
         connection.Close();
 
-        return order;
+        return pedido;
     }
 
-    public Order GetById(int id) {
+    public Pedido GetById(int id) {
         var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Order_tb WHERE (order_id = $order_id)";
-        command.Parameters.AddWithValue("$order_id", id);
+        command.CommandText = "SELECT * FROM Pedidos WHERE (pedId = $pedId)";
+        command.Parameters.AddWithValue("$pedId", id);
 
-        var reader = command.ExecuteReader();
+        var pedido = command.ExecuteReader();
         reader.Read();
 
-        var Order = ReaderToOrder(reader);
+        var Pedido = ReaderToPedido(reader);
 
         connection.Close();
-        return Order;
+        return pedido;
     }
 
-    public Order Update(Order order) {
+    public Pedido Update(Pedido pedido) {
         var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Order_tb VALUES ($order_id, $employee_id, $order_date, $order_weigth, $carrier_code, $customer_id)";
+        command.CommandText = "INSERT INTO Pedido VALUES ($pedId, $employee_id, $order_date, $order_weigth, $carrier_code, $customer_id)";
         command.CommandText = "UPDATE Orders_tb SET employee_id = $employee_id, order_date = $order_date, order_weigth = $order_weigth,  carrier_code = $carrier_code, customer_id = $customer_id WHERE (order_id = $order_id)";
         command.Parameters.AddWithValue("$order_id", order.OrderId);
         command.Parameters.AddWithValue("$employee_id", order.EmployeeId);
@@ -111,7 +110,7 @@ class OrderRepository {
         connection.Open();
 
         var command = connection.CreateCommand();
-        command.CommandText = "SELECT count(id) FROM Order_tb WHERE (order_id = $order_id)";
+        command.CommandText = "SELECT count(id) FROM Pedido WHERE (Pedido = $pedId)";
         command.Parameters.AddWithValue("$id", id);
 
         var reader = command.ExecuteReader();
@@ -121,8 +120,8 @@ class OrderRepository {
         return result;
     }
 
-    private Order ReaderToOrder(SqliteDataReader reader) {
-        var order = new Order(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5));
-        return order;
+    private Pedido ReaderToPedido(SqliteDataReader reader) {
+        var pedido = new Pedido(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3), reader.GetInt32(4), reader.GetInt32(5));
+        return pedido;
     }
 }

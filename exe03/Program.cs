61dd5e2d0f6d73dd2eced8f78d72944c71f2e840
biarossih <101606116+biarossih@users.noreply.github.com>
2023-06-06 23:56:43 -0300

    Program.cs

--- exe03/Program.cs
+++ exe03/Program.cs
@@ -1,12 +1,12 @@
-using Microsoft.Data.Sqlite;
-using DBlesson.Database;
+using exe03.Database;
+using exe03.Models;
+using Microsoft.Data.Sqlite;;
 using DBlesson.Repositories;
-using DBlesson.Models;
 
 var dbConfig = new DatabaseConfig();
 var dbSetup = new DatabaseSetup(dbConfig);
-var customerRepository = new CustomerRepository(dbConfig);
-var orderRepository = new OrderRepository(dbConfig);
+var clienteRepository = new ClienteRepository(dbConfig);
+var pedidoRepository = new PedidoRepository(dbConfig);
 
 var modelName = args[0];
 var modelAction = args[1];
@@ -14,26 +14,26 @@ var modelAction = args[1];
 if (modelName.ToLower() == "customer") {
     if (modelAction.ToLower() == "list") {
         Console.WriteLine("Customer List");
-        foreach ( var customer in customerRepository.GetAll()) {
-            Console.WriteLine($"{customer.Id}, {customer.Address}, {customer.City}, {customer.Region}, {customer.ZipCode}, {customer.Country},  {customer.Phone}");
+        foreach ( var cliente in clienteRepository.GetAll()) {
+            Console.WriteLine($"{cliente.ClienteId}, {cliente.Endereco}, {cliente.Cidade}, {cliente.Regiao}, {cliente.CodigoPostal}, {cliente.Pais},  {cliente.Telefone}");
         }
     }else if (modelAction.ToLower() == "insert") {
-        Console.WriteLine("Insert new Customer");
+        Console.WriteLine("Insert new Cliente");
         var id = Convert.ToInt32(Console.ReadLine());
-        string address = Console.ReadLine();
-        string city = Console.ReadLine();
-        string region = Console.ReadLine();
-        string zipcode = Console.ReadLine();
-        string country = Console.ReadLine();
-        string phone = Console.ReadLine();
-        var customer = new Customer(id, address, city, region, zipcode, country, phone);
-        customerRepository.Save(customer);
+        string endereco = Console.ReadLine();
+        string cidade = Console.ReadLine();
+        string regiao = Console.ReadLine();
+        string codigoPostal = Console.ReadLine();
+        string pais = Console.ReadLine();
+        string telefone = Console.ReadLine();
+        var cliente = new Cliente(id, endereco, cidade, regiao, codigoPostal, pais, telefone);
+        clienteRepository.Save(cliente);
 
     }
-}else if (modelName.ToLower() == "order") {
+}else if (modelName.ToLower() == "pedido") {
     if(modelAction.ToLower() == "list") {
-        Console.WriteLine("Orders List");
-        foreach (var order in orderRepository.GetAll()) {
+        Console.WriteLine("Pedidos List");
+        foreach (var pedido in pedidoRepository.GetAll()) {
             Console.WriteLine($"{order.OrderId}, {order.EmployeeId}, {order.OrderDate}, {order.Weigth}, {order.CarrierCode}, {order.CustomerId}");
         }
     }else if (modelAction.ToLower() == "insert") {
